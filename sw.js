b7288d8b05be8b3f314e93d53e477df822886d58
solooo <romeyeaston@hotmail.com>
2025-05-25 13:36:13 -0400

    OH SHIIIIII

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-
+// Basic service worker to enable PWA install
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
